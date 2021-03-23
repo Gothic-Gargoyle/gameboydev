3d2a76ce3e5b863fb973436d8335d43e17f1b951
Hylbren Rijnders <hylbrenrijnders@hotmail.com>
2021-03-23 01:27:14 +0100

    init commit                                                             - Working sprite move                                                             - Working Background

--- src/main.c
+++ src/main.c
@@ -1,67 +1,73 @@
 #include <stdio.h>
-#include "D:\programmeren\cenasm\Gameboy\gbdk\include\gb\gb.h"
-#include "simplebackground.c"
-#include "simplebackgroundmap.c"
-#include "plenemy.c"
+
+#include <gb/gb.h>
+
+#include <gb/font.h>
+
+// Tiles & stuff
+#include "tiles/env/simplebackground.c"
+#include "tiles/env/simplebackgroundmap.c"
+#include "tiles/sprites/plenemy.c"
+#include "physics/physics.c"
+#include "input/input.c"
+
 
 void init();
 void checkInput();
 void updateSwitches();
+font_t min_font;
+
+
+//Not sure sabout this one tho
+void performantdelay(UINT8 numloops){
+    UINT8 i;
+    for (i =0; i < numloops;i++){
+        wait_vbl_done();
+    }
+}
 
 int main() {
-
     init();
 
     while (1){
 
         checkInput();
         updateSwitches();
-        wait_vbl_done();
+        performantdelay(5);
 
 
     }
 }
 
 void init(){
-    DISPLAY_ON;
-    //set background
-    set_bkg_data(0,11,backgroundtiles);
-    set_bkg_tiles(0,0,40,18,backgroundmap);
-    SHOW_BKG;
+
+    font_init();
+    min_font = font_load(font_min); //36 tiles
+    font_set(min_font);
+
+    //set headsupdisplay
+
 
     //set sprites
-    set_sprite_data(0,2,plenemy);
+    set_sprite_data(0,3,plenemy);
     set_sprite_tile(0,0);
-    move_sprite(0,88,87);
+    playerlocation[0] = 88;
+    playerlocation[1] = floorYposition;
+    move_sprite(0,playerlocation[0],playerlocation[1]);
+    jumping = 0;
+    running = 0;
+    currentSpeedX = 2;
 
-
-
+    //set background
+    set_bkg_data(37,11,backgroundtiles);
+    set_bkg_tiles(0,0,40,18,backgroundmap);
+    DISPLAY_ON;
 }
 
 void updateSwitches(){
     HIDE_WIN;
     SHOW_SPRITES;
-
+    SHOW_BKG;
 }
 
 
-void checkInput() {
-
-    switch (joypad()){
-        case J_LEFT:
-            scroll_sprite(0,-1,0); // ?, X, Y
-            break;
-
-        case J_RIGHT:
-            scroll_sprite(0,1,0);
-            break;
-
-        case J_UP:
-            scroll_sprite(0,0,-1);
-            break;
-
-        case J_DOWN:
-            scroll_sprite(0,0,1);
-            break;
-    }
-}
